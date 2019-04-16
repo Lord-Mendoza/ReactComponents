dcb41a8da53e40608125508411a3ad94706cc4b2
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-04-16 15:46:00 -0400

    Initial commit; grid component almost complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Button} from 'react-bootstrap';
+
+import GridComponent from "./GridComponent";
+
+const labels1 = ["First Name", "Last Name", "Age"];
+const data1 = [{"First Name": "Lord", "Last Name": "Mendoza", "Age": 24},
+    {"First Name": "Daniel", "Last Name": "Kim", "Age": 26},
+    {"First Name": "Kiran", "Last Name": "Adepu", "Age": 28},
+    {"First Name": "Nikunj", "Last Name": "Patel", "Age": 40}];
+
+const labels2 = ["Make", "Model", "Color"];
+const data2 = [{"Make": "Honda", "Model": "Civic", "Color": "Black"},
+    {"Make": "Honda", "Model": "Accord", "Color": "Blue"},
+    {"Make": "Toyota", "Model": "Corolla", "Color": "Silver"},
+    {"Make": "Tesla", "Model": "Model S", "Color": "Silver"}];
+
+const sampleData = (len) => {
+    let retVal = [];
+    for (let i = 0; i < len; i++){
+        retVal.push({"Make": "Make" + i, "Model": "Model" + i, "Color": "Color" + i});
+    }
+    return retVal;
+};
+
+const pageSizing = [10, [10, 25, 30]];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+
+        this.state = {
+            labels: labels2,
+            data: sampleData(100),
+            pageSizing: pageSizing
+        };
+
+        this.changeData = this.changeData.bind(this);
+    }
+
+    changeData() {
+        if (this.state.change)
+            this.setState({change: false, labels: labels2, data: data2});
+        else
+            this.setState({change: true, labels: labels1, data: data1});
+    }
+
+    render() {
+        return (
+            <div>
+                {/*<Button variant="danger" onClick={this.changeData}>
+                    Switch
+                </Button>*/}
+
+                <GridComponent columns={this.state.labels}
+                               rows={this.state.data}
+                               pageSizing={this.state.pageSizing}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/GridComponent.js
@@ -0,0 +1,200 @@
+/*
+Lord Mendoza - 4/16/19
+
+The grid component takes a list of columns & generates a fully-featured grid that can be interacted
+with by the user. Several functions are available such as sorting, paginating, adjusting column
+widths, and adding/updating/deleting (including multi-delete)/searching/refreshing.
+
+Contains the following props (required ones are indicated with (r))
+1) (r) "columns" = takes an array of strings
+	[
+		"<name of column>", ...
+	]
+
+2) (r) "rows" = takes a Json array
+	[
+		{<name of corresponding column>: <column value>, <next column>: <column value>, ...},
+		...
+	]
+Note: for proper sorting behavior, ensure to pass numbers as column value for number-typed columns, rather
+than "10", or "11".
+
+2) (r) "showControls" = takes a boolean
+3) "defaultPageSize" = takes a number; set to 10 by default
+4) "showSelectBtn" = takes a boolean; set to false by default
+5) "pageSizes" = takes an array of numbers; set to [10, 50, 100] by default
+
+Sample usage:
+	<GridComponent columns={varJsonArray}
+				   showControls=true
+				   defaultPageSize=25
+				   showSelectBtn=true
+				   pageSizes=[25, 50, 100]
+	   />
+ */
+//======================================================================================================================
+//============================================= IMPORTS ================================================================
+
+//React
+import React, {Component} from 'react';
+import './GridComponent.css';
+
+//DevExpress Grid
+import {
+    Grid, PagingPanel,
+    Table, TableColumnResizing,
+    TableHeaderRow,
+} from '@devexpress/dx-react-grid-bootstrap4';
+import "@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css";
+import {IntegratedPaging, IntegratedSorting, PagingState, SortingState} from "@devexpress/dx-react-grid";
+
+//======================================================================================================================
+//================================= GLOBAL VARIABLES FOR GRID CONFIGURATION ============================================
+
+const TableComponent = ({...restProps}) => (
+    <Table.Table
+        {...restProps}
+        className="table-striped"
+    />
+);
+
+//======================================================================================================================
+//=========================================== START OF CLASS ===========================================================
+
+class GridComponent extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            columns: [],
+            rows: [],
+            columnWidths: [],
+            pageSize: 10,
+            pageSizes: [10, 50, 100],
+            currentPage: 0
+        };
+
+        this.changeSorting = sorting => this.setState({sorting});
+        this.changeColumnWidths = (columnWidths) => {this.setState({columnWidths})};
+        this.changeCurrentPage = currentPage => this.setState({currentPage});
+        this.changePageSize = pageSize => this.setState({pageSize});
+    }
+
+    componentDidMount() {
+        const {columns, rows, pageSizing} = this.props;
+
+        //Setting up the columns
+        let gridColumns = columns.map(v => {
+            return {name: v.replace(/\s/g, ""), title: v};
+        });
+
+        //Setting up the rows
+        let gridRows = [];
+        rows.forEach(v => {
+            let gridRow = {};
+
+            columns.forEach(p => {
+                if (v.hasOwnProperty(p)) {
+                    gridRow[p.replace(/\s/g, "")] = v[p];
+                }
+            });
+
+            gridRows.push(gridRow);
+        });
+
+        //Setting up the page sizes for paging
+        let pageSize = 10;
+        let pageSizes = [10, 50, 100];
+        if (pageSizing !== undefined) {
+            if (pageSizing.length === 2) {
+                pageSize = pageSizing[0];
+                pageSizes = pageSizing[1];
+            }
+        }
+
+        this.setState({columns: gridColumns, rows: gridRows, pageSize, pageSizes});
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.columns !== prevProps.columns || this.props.rows !== prevProps.rows) {
+            const {columns, rows, pageSizing} = this.props;
+
+            //Setting up the columns
+            let gridColumns = columns.map(v => {
+                return {name: v.replace(/\s/g, ""), title: v};
+            });
+
+            //Setting up the rows
+            let gridRows = [];
+            rows.forEach(v => {
+                let gridRow = {};
+
+                columns.forEach(p => {
+                    if (v.hasOwnProperty(p)) {
+                        gridRow[p.replace(/\s/g, "")] = v[p];
+                    }
+                });
+
+                gridRows.push(gridRow);
+            });
+
+            //Setting up the page sizes for paging
+            let pageSize = 10;
+            let pageSizes = [10, 50, 100];
+            if (pageSizing !== undefined) {
+                if (pageSizing.length === 2) {
+                    pageSize = pageSizing[0];
+                    pageSizes = pageSizing[1];
+                }
+            }
+
+            this.setState({columns: gridColumns, rows: gridRows, pageSize, pageSizes});
+        }
+    }
+
+    render() {
+        const {rows, columns, sorting, columnWidths, pageSize, pageSizes, currentPage} = this.state;
+
+        return (
+            <div style={{fontSize: '12px'}}>
+
+                <Grid
+                    rows={rows}
+                    columns={columns}
+                >
+                    <SortingState
+                        sorting={sorting}
+                        onSortingChange={this.changeSorting}
+                    />
+
+                    <PagingState
+                        currentPage={currentPage}
+                        onCurrentPageChange={this.changeCurrentPage}
+                        pageSize={pageSize}
+                        onPageSizeChange={this.changePageSize}
+                    />
+
+                    <IntegratedPaging/>
+                    <IntegratedSorting/>
+                    <Table
+                        tableComponent={TableComponent}
+                    />
+
+                    <TableColumnResizing
+                        columnWidths={columnWidths}
+                        onColumnWidthsChange={this.changeColumnWidths}
+                    />
+
+                    <TableHeaderRow showSortingControls/>
+
+                    <PagingPanel
+                        pageSizes={pageSizes}
+                    />
+                </Grid>
+
+            </div>
+        );
+    }
+}
+
+export default GridComponent;
