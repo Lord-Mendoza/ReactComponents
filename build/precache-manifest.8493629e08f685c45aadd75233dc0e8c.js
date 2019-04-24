f46c4496983e10e83be9b538032ed2813025f5aa
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-04-24 09:11:28 -0400

    restricting upload to build file only

---
+++ build/precache-manifest.8493629e08f685c45aadd75233dc0e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b546795486c61818ea4a",
+    "url": "/static/js/main.e2d88a34.chunk.js"
+  },
+  {
+    "revision": "bf942f918758ed7e8f7b",
+    "url": "/static/js/2.d3e25801.chunk.js"
+  },
+  {
+    "revision": "b546795486c61818ea4a",
+    "url": "/static/css/main.8df2c231.chunk.css"
+  },
+  {
+    "revision": "bf942f918758ed7e8f7b",
+    "url": "/static/css/2.d2fd671a.chunk.css"
+  },
+  {
+    "revision": "35530ce60af8d91b7674f6872fd32d6d",
+    "url": "/index.html"
+  }
+];
