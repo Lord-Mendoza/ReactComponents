dc3ad6bd11c9907ddeedf68a8d995c72cc4ff2b6
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-04-24 11:04:57 -0400

    Mask example complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import MaskComponent from "./MaskComponent";
+
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        this.state = {
-
-        };
     }
 
     render() {
-
-
         return (
             <div>
-
+                <MaskComponent header={"Please Wait"}
+                               content={"Loading..."}
+                               show={true}
+                               loadingIcon={true}/>
             </div>
         );
     }
